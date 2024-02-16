d37feb26a2205770d9cc107647518133d14a2405
Fabricio Bravo Guevara <fabricio.bravo0@gecgithub01.walmart.com>
2024-02-16 18:11:52 -0300

    adding merge sort algorithm in golang

---
+++ sorting_arrays/merge_sort.go
@@ -0,0 +1,47 @@
+package sorting_arrays
+
+func MergeSort(A []int) []int {
+	sorted := Sort(A)
+	return sorted
+}
+
+func Sort(A []int) []int {
+	if len(A) < 2 {
+		return A
+	}
+	left, right := Split(A)
+	left = Sort(left)
+	right = Sort(right)
+	merge := Merge(left, right)
+	return merge
+}
+
+// get left and right sorted arrays
+func Merge(left []int, right []int) []int {
+	merge := make([]int, len(left)+len(right))
+	l, r := 0, 0
+	for i := 0; i < len(merge); i++ {
+		if l >= len(left) {
+			merge[i] = right[r]
+			r++
+			continue
+		} else if r >= len(right) {
+			merge[i] = left[l]
+			l++
+			continue
+		}
+		if left[l] <= right[r] {
+			merge[i] = left[l]
+			l++
+		} else {
+			merge[i] = right[r]
+			r++
+		}
+	}
+	return merge
+}
+
+func Split(A []int) ([]int, []int) {
+	left, right := A[0:(len(A)/2)], A[(len(A)/2):]
+	return left, right
+}
---
+++ sorting_arrays/merge_sort_test.go
@@ -0,0 +1,45 @@
+package sorting_arrays
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMergeSort(t *testing.T) {
+	t.Run("Given A = {8, 7, 9, 2, 3, 1, 5, 4, 2, 6}, then the function should return {1, 2, 2, 3, 4, 5, 6, 7, 8, 9}", func(t *testing.T) {
+		a := []int{8, 7, 9, 2, 3, 1, 5, 4, 2, 6}
+		expected := []int{1, 2, 2, 3, 4, 5, 6, 7, 8, 9}
+
+		got := MergeSort(a)
+
+		assert.Equal(t, expected, got)
+
+	})
+	t.Run("Given A = {4,9,2,4,7,6}, then the function should return {2, 4, 4, 6, 7, 9}", func(t *testing.T) {
+		a := []int{4, 9, 2, 4, 7, 6}
+		expected := []int{2, 4, 4, 6, 7, 9}
+
+		got := MergeSort(a)
+
+		assert.Equal(t, expected, got)
+
+	})
+	t.Run("Given A = {4,9,2,4,7,6}, then the function should return {2, 4, 4, 6, 7, 9}", func(t *testing.T) {
+		a := []int{4, 9, 2}
+		expected := []int{2, 4, 9}
+
+		got := MergeSort(a)
+
+		assert.Equal(t, expected, got)
+
+	})
+	t.Run("Given A = {38,27,43,3,9,82,10}, then the function should return {3,9,10,27,38,43,82}", func(t *testing.T) {
+		a := []int{38, 27, 43, 3, 9, 82, 10}
+		expected := []int{3, 9, 10, 27, 38, 43, 82}
+
+		got := MergeSort(a)
+
+		assert.Equal(t, expected, got)
+
+	})
+}
